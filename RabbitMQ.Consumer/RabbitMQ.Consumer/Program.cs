2550354bfd1733008a2794d437862a55faaed804
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-06-04 17:35:06 +0300

    Basic level publisher and consumer applications were created.

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿//Bağlantı oluşturma
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://ctpgaytw:kMPeUt-BVWJT4h_zrgcNESx5-Eyp4bAa@sparrow.rmq.cloudamqp.com/ctpgaytw");
+
+//Bağlantıyı açma ve kanal oluşturma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma
+//Consumerdaki kuyruk publisherdaki ile birebir aynı yapıda tanımlanmalıdır. 
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queuedan mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+	//kuyruğa gelen mesajın işlendiği yerdir.
+	//e.Body: Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+	//e.Body.Span veya e.Body.ToArray(): Kuyruktaki mesajın byte verisini getirecektir.
+	Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://ctpgaytw:kMPeUt-BVWJT4h_zrgcNESx5-Eyp4bAa@sparrow.rmq.cloudamqp.com/ctpgaytw");
+
+//Bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection =factory.CreateConnection();
+using IModel channel=connection.CreateModel();
+
+//Queue(mesajın gönderileceği kuyruk) oluşturma 
+//Bir kuyruk exclusive:true olarak ayarlanıyorsa, o kuyruk o bağlantıya özel oluşturulur ve daha sonra imha edilir.Yani consumer kuyruğa erişemez.
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Kuyruğa mesaj gönderme
+//RabbitMQ kuyruğa atacağı mesajları byte türüenden kabul eder. Haliyle gönderilecek mesajları byte türüne dönüştürmemiz gerekir.
+byte [] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange:"",routingKey: "example-queue", body:message);
+
+Console.Read();
