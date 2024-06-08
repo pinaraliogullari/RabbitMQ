9d6fe23a0c7b4c8fa55891e84a4509272c17c64f
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-06-08 15:14:16 +0300

    Topic Exchange example is completed.

---
+++ RabbitMQ.TopicExchange.Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://hivihrwt:4yeSDzme81G9-i1hzgFDl_TBE7ctvqjg@sparrow.rmq.cloudamqp.com/hivihrwt");
+
+using IConnection connection= factory.CreateConnection();
+using IModel channel= connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+Console.Write("Dinlecek topic formatını belirtiniz: ");
+string topic= Console.ReadLine();
+string queueName= channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName, exchange: "topic-exchange-example", routingKey: "topic");
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+	string message = Encoding.UTF8.GetString(e.Body.Span);
+	Console.WriteLine(message);
+};
+Console.Read();
---
+++ RabbitMQ.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Unicode;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://hivihrwt:4yeSDzme81G9-i1hzgFDl_TBE7ctvqjg@sparrow.rmq.cloudamqp.com/hivihrwt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel=connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+for (int i = 0; i < 100; i++)
+{
+	await Task.Delay(200);
+	byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+	Console.Write("Topic giriniz: ");
+	string topic=Console.ReadLine();
+
+	channel.BasicPublish(exchange:"topic-exchange-example",routingKey: topic,body:message);
+
+	Console.Read();
+}
